c7c71f61143e845240cc28468a07baae2863a506
Mohammad Abdul Gafoor <124916666+abdul-1432@users.noreply.github.com>
2023-10-10 16:35:08 +0530

    Add files via upload

---
+++ 6.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int num1=0;
+		int num2=1;
+		int num3=3;
+		int count = 5;
+		System.out.println(num1 + ""+num2);
+		for(int i=2;i<count;i++)
+		{
+		    num3=num1+num2;
+		    System.out.println(num3);
+		    num1=num2;
+		    num2=num3;
+		}
+	}
+}
