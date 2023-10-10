b743e0c74936f91427a496e2a499489751b64659
Mohammad Abdul Gafoor <124916666+abdul-1432@users.noreply.github.com>
2023-10-10 16:34:51 +0530

    Add files via upload

---
+++ 5.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int i=90;
+		{
+		    if(i%2!=0)
+		    {
+		    System.out.println(i+ " Is Odd");
+		    }
+		    else
+		    {
+		    System.out.println(i+ " Is Even");
+		    }
+		}
+		 
+		}
+	}
